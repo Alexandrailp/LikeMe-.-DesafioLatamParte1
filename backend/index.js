d0e95ec98c0019915555da66efdcca179eabe449
Alexandra Lara <alexandrailp.r@gmail.com>
2026-08-14 18:40:59 -0400

    Se agrega la ruta GET

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,16 @@ const pool = new Pool({
 app.use(cors()); 
 app.use(express.json()); 
 
+app.get('/posts', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM posts');
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Error al obtener los posts:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`¡Servidor encendido y escuchando en el puerto ${PORT}!`);
 });
